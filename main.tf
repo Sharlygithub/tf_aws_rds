3e79675f99e20444d8506e5db9cae6a90de7d9b1
Sharlygithub <147545681+Sharlygithub@users.noreply.github.com>
2023-12-19 17:03:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-//
-// Module: tf_aws_rds
-//
-
-// This template creates the following resources
-// - An RDS instance
-// - A database subnet group
-// - You should want your RDS instance in a VPC
-
 resource "aws_db_instance" "main_rds_instance" {
   identifier        = "${var.rds_instance_identifier}"
   allocated_storage = "${var.rds_allocated_storage}"
@@ -19,33 +10,27 @@ resource "aws_db_instance" "main_rds_instance" {
 
   port = "${var.database_port}"
 
-  # Because we're assuming a VPC, we use this option, but only one SG id
   vpc_security_group_ids = ["${aws_security_group.main_db_access.id}"]
 
-  # We're creating a subnet group in the module and passing in the name
   db_subnet_group_name = "${aws_db_subnet_group.main_db_subnet_group.id}"
   parameter_group_name = "${var.use_external_parameter_group ? var.parameter_group_name : aws_db_parameter_group.main_rds_instance.id}"
 
-  # We want the multi-az setting to be toggleable, but off by default
   multi_az            = "${var.rds_is_multi_az}"
   storage_type        = "${var.rds_storage_type}"
   iops                = "${var.rds_iops}"
   publicly_accessible = "${var.publicly_accessible}"
 
-  # Upgrades
   allow_major_version_upgrade = "${var.allow_major_version_upgrade}"
   auto_minor_version_upgrade  = "${var.auto_minor_version_upgrade}"
   apply_immediately           = "${var.apply_immediately}"
   maintenance_window          = "${var.maintenance_window}"
 
-  # Snapshots and backups
   skip_final_snapshot   = "${var.skip_final_snapshot}"
   copy_tags_to_snapshot = "${var.copy_tags_to_snapshot}"
 
   backup_retention_period = "${var.backup_retention_period}"
   backup_window           = "${var.backup_window}"
 
-  # enhanced monitoring
   monitoring_interval = "${var.monitoring_interval}"
 
   tags = "${merge(var.tags, map("Name", format("%s", var.rds_instance_identifier)))}"
@@ -56,18 +41,6 @@ resource "aws_db_parameter_group" "main_rds_instance" {
 
   name   = "${var.rds_instance_identifier}-${replace(var.db_parameter_group, ".", "")}-custom-params"
   family = "${var.db_parameter_group}"
-
-  # Example for MySQL
-  # parameter {
-  #   name = "character_set_server"
-  #   value = "utf8"
-  # }
-
-
-  # parameter {
-  #   name = "character_set_client"
-  #   value = "utf8"
-  # }
 
   tags = "${merge(var.tags, map("Name", format("%s", var.rds_instance_identifier)))}"
 }
@@ -80,7 +53,6 @@ resource "aws_db_subnet_group" "main_db_subnet_group" {
   tags = "${merge(var.tags, map("Name", format("%s", var.rds_instance_identifier)))}"
 }
 
-# Security groups
 resource "aws_security_group" "main_db_access" {
   name        = "${var.rds_instance_identifier}-access"
   description = "Allow access to the database"
